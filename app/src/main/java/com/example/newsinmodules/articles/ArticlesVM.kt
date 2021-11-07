bffd06d6898edc5ac1085249c839f15dbdb82220
Indira Temkesheva <indiratemk@gmail.com>
2021-11-07 19:04:55 +0300

    add view model

---
+++ app/src/main/java/com/example/newsinmodules/articles/ArticlesVM.kt
@@ -0,0 +1,56 @@
+package com.example.newsinmodules.articles
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.newsinmodules.network.NewsAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
+
+class ArticlesVM(
+    private val newsApi: NewsAPI
+) : ViewModel() {
+
+    private var _articles = MutableLiveData<List<Article>>()
+    val articles: LiveData<List<Article>>
+        get() = _articles
+    
+    private var _error = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = _error
+    
+    fun getArticles() {
+        newsApi.getArticles().enqueue(object : Callback<ArticlesResponse> {
+            override fun onResponse(
+                call: Call<ArticlesResponse>,
+                response: Response<ArticlesResponse>
+            ) {
+                if (response.isSuccessful && response.body() != null) {
+                    response.body()?.articles?.let { articles ->
+                        _articles.value = articles
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ArticlesResponse>, t: Throwable) {
+                // TODO: 07.11.2021 Вынести в ресурсы с исп. интерфейса 
+                _error.value = "Что-то пошло не так..."
+            }
+        })
+    }
+
+    class Factory @Inject constructor(
+        private val newsApi: NewsAPI
+    ) : ViewModelProvider.Factory {
+
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            // TODO: 07.11.2021 not working!!! 
+//            require(modelClass == ArticlesVM::class)
+            return ArticlesVM(newsApi) as T
+        }
+    }
+}
--- app/src/main/java/com/example/newsinmodules/articles/MainActivity.kt
+++ app/src/main/java/com/example/newsinmodules/articles/MainActivity.kt
@@ -2,66 +2,55 @@ package com.example.newsinmodules.articles
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsinmodules.NewsApp
 import com.example.newsinmodules.databinding.MainActivityBinding
 import com.example.newsinmodules.details.ArticleDetailsActivity
-import com.example.newsinmodules.network.NewsAPI
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var newsAPI: NewsAPI
-    
     private lateinit var binding: MainActivityBinding
+    private val viewModel: ArticlesVM by viewModels {
+        factory
+    }
+
+    @Inject
+    lateinit var factory: ArticlesVM.Factory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         (application as NewsApp).appComponent.inject(this)
 
-        newsAPI.getArticles().enqueue(object : Callback<ArticlesResponse> {
-            override fun onResponse(
-                call: Call<ArticlesResponse>,
-                response: Response<ArticlesResponse>
-            ) {
-                if (response.isSuccessful && response.body() != null) {
-                    response.body()?.articles?.let { articles ->
-                        val articlesAdapter = ArticlesAdapter(
-                            articles,
-                            object : ArticleClickListener {
-                                override fun onArticleClick(article: Article) {
-                                    ArticleDetailsActivity.startActivity(
-                                        this@MainActivity,
-                                        article
-                                    )
-                                }
-                            }
+        viewModel.articles.observe(this, { articles ->
+            val articlesAdapter = ArticlesAdapter(
+                articles,
+                object : ArticleClickListener {
+                    override fun onArticleClick(article: Article) {
+                        ArticleDetailsActivity.startActivity(
+                            this@MainActivity,
+                            article
                         )
-                        binding.rvNews.apply {
-                            layoutManager = LinearLayoutManager(this@MainActivity)
-                            adapter = articlesAdapter
-                            setHasFixedSize(true)
-                        }
                     }
                 }
-            }
-
-            override fun onFailure(call: Call<ArticlesResponse>, t: Throwable) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "Что-то пошло не так =(",
-                    Toast.LENGTH_SHORT
-                ).show()
+            )
+            binding.rvNews.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = articlesAdapter
+                setHasFixedSize(true)
             }
         })
+
+        viewModel.error.observe(this, { error ->
+            Toast.makeText(this@MainActivity, error, Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.getArticles()
     }
 }
